15f6545607a903a330a81a2406221fbaa27a8c2f
Liddle <liddle@iastate.edu>
2022-11-15 14:33:53 -0600

    Completed

--- ultimateTODOLIST.py
+++ ultimateTODOLIST.py
@@ -1,25 +1,23 @@
 # David Liddle 11/11/2022
 # COM S 127 - Assignment 5
 
-#X1 init list
-#X2 save list
-#X3 load list
-#X4 check item
-#X5 add item
-#X6 delete item
-#X7 move item
-# 8 printTODOList
-# 9 run application
-# 10 main
-
+# add a clear function to clear the screen between transitions from the main menu and the application menu
 
 import pickle, sys
+from os import system, name
+from time import sleep
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 # This function initializes a todolist as a python dicitonary with 5 sub categories of a list
 def initList():
     todoList = {}
     todoList['backlog'] = []
-    todoList['todo']
+    todoList['todo'] = []
     todoList['in_progress'] = []
     todoList['in_review'] = []
     todoList['done'] = []
@@ -75,8 +73,8 @@ def addItem(item, toList, todoList):
 # Delete item if itemFound == true
 def deleteItem(item, todoList):
     itemFound, keyName, index = checkItem(item, todoList)
-    if (is_in_todolist):
-        todoList[x].pop(y)
+    if (itemFound):
+        todoList[keyName].pop(index)
     else:
         print(f'The item: {item} was not found.')
 
@@ -86,44 +84,78 @@ def deleteItem(item, todoList):
 def moveItem(item, toList, todoList):       
     itemFound, keyName, index = checkItem(item, todoList)
     if (keyName in todoList):
-    addItem(item, toList, todoList)
-    deleteItem(item, todolist)
-
+        addItem(item, toList, todoList)
+        deleteItem(item, todoList)
+# Prints todoList dictionary for the user to see
 def printTODOList(todoList):
-    for keys in example_dict:
-        print(f'{keys}: {example_dict[keys]}')
+    for keys in todoList:
+        print(f'{keys}: {todoList[keys]}')
 
+# This function executes the Application Menu
 def runApplication(todoList):
+    clear()
     while True:
         print(66*'-')
-        choice = input("APPLICATION MENU: [a]dd to backlog, [m]ove item, [s]ave list, or [q]uit to main menu?: ")
-        prine()
+        choice = input("APPLICATION MENU: \n\ta: add ITEM to list\n\tm: move ITEM\n\td: delete ITEM\n\ts: save list \n\tq: quit to main menu \n")
+        print()
 
         if choice == "a":
-            for keys in example_dict.keys():
-                print('Here are the categories of the list.\n\t' + keys)
-            # create a variable from user input to be added to the list
-            item = input("Enter an item to be added to a category of the list: ")
+            clear()
+            print("TODO list CATEGORIES: ")
+            for keys in todoList.keys():
+                print('\t' + keys)
+            print()
             
-            # Yeah, get some input validation in there.
-            toList = input("Enter the category name: ")
+            # create a variable from user input to be added to the list
+            item = input("Enter an ITEM to be added to a CATEGORY of the list: ")
+            toList = input("Enter the full CATEGORY name: ")
+            keysList = list(todoList.keys())
+            if toList not in keysList:
+                raise Exception("Enter only a CATEGORY that is listed")
 
-            addItem(item, toList)
+            addItem(item, toList, todoList)
+            print()
+            print(f'ITEM: {item} added to CATEGORY {toList}:')
+            print()
             printTODOList(todoList)
+            sleep(3)
+            clear()
         
         elif choice == "m":
+            
             # Check to see if any of the lists in the data structure have items in them.
-            itemsInList = False    
-            for keys in todoList.keys():
-                if todoList[keys] != []:
-                    itemsInList = True
-            # </code>
-            pass
+            item = input("Enter an item name is to be moved: ")  
+            todoListValues = list(todoList.values())
+            todoListValuesAppended = []
+            print(todoListValues)
+            for i in range(0, len(todoListValues)):
+                for j in range(0, len(todoListValues[i])):
+                    todoListValuesAppended.append(todoListValues[i][j])
+            if todoListValuesAppended == []:
+                raise Exception("There are no values to move.")
+            elif item not in todoListValuesAppended:
+                raise Exception("The inputted value is not in the list")     
+            else:
+                print("Here are the categories of the list: ")
+                print()
+                for keys in todoList.keys():
+                    print(keys)
+                print()
+                toList = input("Enter the category of the list the item will be added to. ")
+                print()
+                deleteItem(item, todoList)
+                addItem(item, toList, todoList) 
+                printTODOList(todoList) 
         
         elif choice == "d":
-            # </code>
-            pass
-
+           item = input("Enter the item name that is to be deleted: ")
+           print(f'ITEM: {item} deleted.')
+           deleteItem(item, todoList)
+           print()
+           printTODOList(todoList)
+           sleep(3)
+           clear() 
+        
         elif choice == "s":
             saveList(todoList)
             print("Saving List...")
@@ -131,59 +163,83 @@ def runApplication(todoList):
             printTODOList(todoList)
 
         elif choice == "q":
-            # </code>
            print("Returning to MAIN MENU...")
            print()
+           main()
+           break
 
     return todoList
 
-# Main
+# Main and Main Menu
 def main():
     taskOver = False
-
+    clear()
     print("The ultimate TODO List")
     print()
-
+    print("    %%%")
+    print("   =====")
+    print("  &%&%%%&")
+    print("  %& < <%")
+    print("   &\__/")
+    print("    \ |____")
+    print("   .', ,  ()")
+    print("  / -.  _)|") 
+    print(" |_(_.    |")
+    print(" '-'\  )  |")
+    print(" mrf )    |")
+    print("    /  .  ).")
+    print("   /    _. |")
+    print(" /'---':.-'|")
+    print("(__.' /    /")
+    print(" \   ( /  /")
+    print("  \ /  _  |") 
+    print("   \  |  '|")
+    print("   | . \  |")
+    print("   |(     |") 
+    print("   |  \ \ |")
+    print("    \  )\ |")
+    print("   __)/ / \\")
+    print("--*--(_.Ooo*----")
+    print()
     print("By: David Liddle")
     print("[COM S 127 B]")
     print()
+    sleep(2)
+    clear()
 
-while taskOver == False:
-    print(66*'-')
-    choice = input("MAIN MENU: [n]ew list, [l]oad list, or [q]uit?: ")
-    print()
+    while taskOver == False:
+        print(66*'-')
+        choice = input("MAIN MENU \n\tn: new list \n\tl: load list \n\tq: quit\n")
+        print()
 
-    if choice == "n":
-        todoList = initList()
-        
-        printTODOList(todoList)
-        
-        runApplication(todoList)
+        if choice == "n":
+            todoList = initList()
+            clear()
+            print('NEW TODO LIST INITIALIZED')
+            print(66*'-')
+            print('LIST/CATEGORIES: ')
+            print()
+            printTODOList(todoList)
+            print('...')
+            sleep(2)
+            clear()
+            runApplication(todoList)
     
-    elif chioce == "l":
-        todoList = loadList()
+        elif choice == "l":
+            todoList = loadList()
+            printTODOList(todoList)
+            runApplication(todoList)
 
-        printTODOList(todoList)
-
-        runApplication(todoList)
-
-    elif choice == "q":
-        taskOver = True
-        print("Goodbye!")
-        print()
-
-    else:
-        print("Please enter [n], [l], or [q]...")
-        print()
+        elif choice == "q":
+            taskOver = True
+            print("Goodbye!")
+            print()
+            sleep(2)
+            clear()
+        else:
+            clear()
+            print("Please enter one of the MAIN MENU arguments")
+            print()
 
 if __name__ == "__main__":
     main()
-# Input validation for a python object
-
-
-
-# class the determines entries?
-#    what should the sub attributes of an object be
-
-# What are the specifics of a how data changes when serialized?
-# Plan to learn web assembly
